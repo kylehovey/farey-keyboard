c3fa37b95bc2f429635ad88907d1405b686299f9
kylehovey <khoveyc@gmail.com>
2023-09-03 20:07:34 -0700

    Set up P5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import P5 from 'p5-svelte';
+	import type { Sketch } from 'p5-svelte';
+
+	let width = 55;
+	let height = 55;
+
+	const sketch: Sketch = (p5) => {
+		const resize = () => {
+			p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+		};
+
+		p5.setup = () => {
+			p5.createCanvas(800, 800);
+			resize();
+		};
+
+		p5.windowResized = () => resize;
+
+		p5.draw = () => {
+			p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+		};
+	};
+</script>
+
+<label>
+	Width
+	<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
+	{width.toFixed(2).padEnd(10, ' ')}
+</label>
+
+<label>
+	Height
+	<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
+	{height.toFixed(2).padEnd(10, ' ')}
+</label>
+
+<P5 {sketch} />
