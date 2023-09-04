86e2b58643be74573bfcfeee3d1d546217db7f6e
kylehovey <khoveyc@gmail.com>
2023-09-03 21:06:46 -0700

    Generating Farey Sequence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,86 @@
 	import P5 from 'p5-svelte';
 	import type { Sketch } from 'p5-svelte';
 
-	let width = 55;
-	let height = 55;
+	interface Location {
+		x: number;
+		y: number;
+	}
+
+	interface FareyDiagram {
+		width: number;
+		height: number;
+		order: number;
+		center: Location;
+	}
+
+	interface Rational {
+		numerator: number;
+		denominator: number;
+		order: number;
+	}
+
+	const r =
+		(_: TemplateStringsArray, ...[numerator, denominator]: number[]) =>
+		(order: number) => ({
+			numerator,
+			denominator,
+			order
+		});
+
+	type P5 = Parameters<Sketch>[0];
+
+	const mediant = (lhs: Rational, rhs: Rational, newOrder: number): Rational =>
+		r`${lhs.numerator + rhs.numerator}/${lhs.denominator + rhs.denominator}`(newOrder);
+
+	const asReal = ({ numerator, denominator }: Rational): number => numerator / denominator;
+
+	const fareySequence = (order: number): Rational[] => {
+		let curOrder = 1;
+		let seq: Rational[] = [r`${0}/${1}`(curOrder), r`${1}/${1}`(curOrder)];
+
+		for (; curOrder <= order; curOrder++) {
+			seq = seq.reduce<Rational[]>(
+				(acc, cur, i) => [
+					...acc,
+					...(i === seq.length - 1 ? [cur] : [cur, mediant(cur, seq[i + 1], curOrder)])
+				],
+				[]
+			);
+		}
+
+		return seq;
+	};
+
+	const fareyDiagram = ({ width, height, order, center: { x: cx, y: cy } }: FareyDiagram) => {
+		const farey = fareySequence(order);
+
+		return (p5: P5) => {
+			const [left__, right_] = [cx - width / 2, cx + width / 2];
+			const [top___, bottom] = [cy - height, cy];
+
+			p5.line(left__, top___, right_, top___);
+			p5.line(left__, bottom, right_, bottom);
+			p5.line(left__, bottom, left__, top___);
+			p5.line(right_, bottom, right_, top___);
+
+			for (const element of farey) {
+				const fx = left__ + asReal(element) * width;
+				const fy = height * 0.9 * (1 / element.order) ** 0.8;
+
+				p5.line(fx, bottom, fx, bottom - fy);
+			}
+		};
+	};
+
+	const f1 = fareyDiagram({
+		width: 900,
+		height: 500,
+		order: 5,
+		center: {
+			x: 500,
+			y: 800
+		}
+	});
 
 	const sketch: Sketch = (p5) => {
 		const resize = () => {
@@ -18,21 +96,9 @@
 		p5.windowResized = () => resize;
 
 		p5.draw = () => {
-			p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+			f1(p5);
 		};
 	};
 </script>
-
-<label>
-	Width
-	<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
-	{width.toFixed(2).padEnd(10, ' ')}
-</label>
-
-<label>
-	Height
-	<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
-	{height.toFixed(2).padEnd(10, ' ')}
-</label>
 
 <P5 {sketch} />
